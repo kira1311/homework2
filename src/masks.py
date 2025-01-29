f98b55a84de78463d05ac2a7804ac99e1f477912
muzhmarova kira <kiramuzhmarova8@gmail.com>
2025-01-29 15:36:07 +0300

    changed masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,4 +1,4 @@
-def get_mask_card_number(card_number: int) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """функция для маскировки номера карты"""
     card_number = str(card_number)
     block1 = card_number[:4]
@@ -9,7 +9,7 @@ def get_mask_card_number(card_number: int) -> str:
     mask_card = f"{block1} {block2}{block3} {block4} {block5}"
     return mask_card
 
-def get_mask_account(account_number: int) -> str:
+def get_mask_account(account_number: str) -> str:
     """функция для маскировки номера счета"""
     block6 = account_number[-4:]
     mask_number = f"**{block6}"
