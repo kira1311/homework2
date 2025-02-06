1429a98e4d0ad1282ae51f54e7a035393d62405d
muzhmarova kira <kiramuzhmarova8@gmail.com>
2025-02-06 10:54:19 +0300

    fix two modules, add new one processing.py and add README.md file

--- src/masks.py
+++ src/masks.py
@@ -9,9 +9,9 @@ def get_mask_card_number(card_number: str) -> str:
     mask_card = f"{block1} {block2}{block3} {block4} {block5}"
     return mask_card
 
+
 def get_mask_account(account_number: str) -> str:
     """функция для маскировки номера счета"""
     block6 = account_number[-4:]
     mask_number = f"**{block6}"
     return mask_number
-
